cdf121e199d6517bffae67e61259855b96382a0d
nmquang1416 <nmquang1416@gmail.com>
2023-11-30 17:32:39 +0700

    update

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function button_demo() {
+    return(
+        <button>Hello world</button>
+    );
+}
+
+export default function button_here() {
+    return(
+        <div>
+            <h1>
+                fucking world
+            </h1>
+            <button_demo/>
+        </div>
+    );
+}
